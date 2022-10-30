63909a2d872cadab9e6ee50c4099d1e860fb930b
Sayarinu <bikabattles@gmail.com>
2022-10-30 16:33:07 -0400

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,3 +1,51 @@
+/*
+Requirements:
+  Must have:
+    Heavy use of variables
+    Custom functions passiong variables as parameters
+    loops
+    control sequences
+    math or random
+    some use of interaction or change over time
+    movement, change, variety
+    conditionals
+    OOP - Methods + passing parameters
+
+    **SHOULD BE GREATER THAN 100 LINES**
+
+  Explore:
+    Color, size, opacity
+    Collection of them are drawn, create patterns and textures
+  
+  Sketch for this Drawing is in the PDF in the main of the project
+
+  Sketch is in 3 different scenes that each tell a different part
+*/
+
+/*
+Author: Carter Garcia
+Creative Coding Midterm Project
+Adjective: Abrasive
+*/
+
+
+/*
+Class and Method Declarations:
+*/
+
+
+// This will be the person in the drawing, they will have their dimensions set by the constructor
+// They will be represented as a circle in the drawing in an almost balloon like feel
+// Scared of being popped
+class Person {
+  constructor(size, color) {
+    this.size = size;
+    this.color = color;
+  }
+}
+
+// Scene and sketch code
+// Scenes will transition based on time
 function setup() {
   createCanvas(800, 800);
   background(0);
@@ -8,24 +56,26 @@ function draw() {
 
 }
 
-function mousePressed(){
-  //letTest()
+function sceneOneSpikes() {
+  const context = canvas.getContext('2d');
 
-}
-/*
-function letTest() {
-  
-  let x = 1;
 
-  {
-    let x = false;  // different variable
-    print(x);  // 2
+  let y = -50;
+  let x = -25;
+  for (let i = 0; i < 11; i++) {
+    context.beginPath();
+    context.moveTo(x, y);
+    context.lineTo(x + 25, y + 100);
+    context.lineTo(x + 50, y);
+    context.fill();
+    context.endPath();
   }
-
-  print(x);  // 1
 }
 
-*/
+
+
+
+
 
 
 
