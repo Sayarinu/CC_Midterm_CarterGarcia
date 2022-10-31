31aece968caf50fba58f0df8778d25d9d9d50e64
Carter Garcia <bikabattles@gmail.com>
2022-10-31 11:21:08 -0400

    Update sketch.js
    Lots of changes and initial draft of the sketch to be presented in class

--- sketch.js
+++ sketch.js
@@ -31,14 +31,48 @@ Adjective: Abrasive
 /*
 Globals:
 */
-
+let angle = 0;
+let explosion = [];
+let explosionPos = [];
+let explosionVel = [];
+let explosionAcc = [];
 /*
 Class Definitions:
 */
-let posX, posY;
 
+/*
+class Explosion {
+  constructor(position, velocity, acceleration) {
+    this.position = position; // Can do this.position.x or y to access values
+    this.velocity = velocity;
+    this.acceleration = acceleration;
+    this.maxspeed = 5;
+  }
 
-class Gear { 
+  display() {
+    noStroke();
+    fill('red');
+    ellipse(this.position.x, this.position.y, 25, 25);
+  }
+
+  update() {
+    this.position.add(this.velocity);
+    if ((this.position.x > width) || (this.position.x < 0)) {
+      this.velocity.x = -this.velocity.x;
+    }
+    if ((this.position.y > height) || (this.position.y < 0)) {
+      this.velocity.y = -this.velocity.y;
+    }
+  }
+
+  accelerate() {
+    this.velocity.mult(1.01);
+    this.velocity.limit(this.maxspeed);
+  }
+}
+*/
+
+class Saw { 
   constructor(x, y, size) {
     this.x = x;
     this.y = y;
@@ -46,13 +80,8 @@ class Gear {
   }
 
   display() {
-    const gearContext = canvas.getContext('2d');
-    ellipse(this.x, this.y, this.size);
-    noStroke();
-    fill(255, 100, 100);
-    gearContext.rect(this.x - 25 - this.size/2, this.y - 25, 50, 50);
-    gearContext.rotate(45/180 * Math.PI);
-    gearContext.rect(this.x - 25 - this.size/2, this.y - 25, 50, 50);
+    rect(this.x, this.y, this.size, this.size);
+    rotate(Math.PI / 6);
   }
 }
 
@@ -99,11 +128,11 @@ class SceneOneSpikes {
       context.lineTo(this.x + 30, this.y + 150 + this.offset);
       context.lineTo(this.x + 60, this.y);
       context.fill();
-      context.closePath();
       this.x += 60;
     }
 
     this.x = -25;
+    
     for (let i = 0; i < 14; i++) {
       context.beginPath();
       context.strokeStyle = 'white';
@@ -111,9 +140,10 @@ class SceneOneSpikes {
       context.lineTo(this.x + 30, this.lowerY - 150 - this.offset);
       context.lineTo(this.x + 60, this.lowerY);
       context.fill();
-      context.closePath();
       this.x += 60;
     }
+
+    ellipse(400, 800, 300, 300);
   }
 
   updateOffset(newOffset) {
@@ -126,6 +156,50 @@ class SceneOneSpikes {
 Function Definitions:
 */
 
+function sceneOne() {
+  background(0);
+  spikes.display();
+  if (millis() < 750) {
+    circularObject(person);
+  } else {
+    fill(255, 0, 0, random(100, 200));
+    ellipse(400, 650, 100, 100);
+    lineFlurry();
+  }
+/*
+  for (let i = 0; i < 100; i+= 1) {
+    explosion[i].update();
+    explosion[i].display();
+  }
+*/
+}
+
+function circularObject(obj) {
+  translate(400, 470);
+  stroke(255);
+  rotate(angle);
+  fill(255);
+  obj.display();
+  angle += 1;
+}
+
+function lineFlurry() {
+  const ctx = canvas.getContext('2d');
+  // set line stroke and line width
+  ctx.strokeStyle = 'white';
+  ctx.lineWidth = 5;
+  
+      // draw a red line
+  for (let i = 0; i < 10; i++) {
+    ctx.beginPath();
+    ctx.moveTo(400, 650);
+    ctx.lineTo(random(200, 600), random(300, 600));
+    ctx.stroke();
+    ctx.closePath();
+  }
+}
+
+/*
 function drawLine(x1, y1, x2, y2, stroke, ctx) {
   ctx.strokeStyle = 'red';
   ctx.lineWidth = stroke;
@@ -149,12 +223,24 @@ function fillCircle(x, y, radius, ctx) {
   ctx.arc(x, y, radius, 0, 2 * Math.PI);
   ctx.fill();
 }
+*/
+person = new Person(100, 100, 100);
+spikes = new SceneOneSpikes(-25, -50, 850, 0);
+
 
 function setup() {
   createCanvas(800, 800);
-  posX = 1;
-  posY = 1;
+  angleMode(DEGREES);
+  background(0);
+  /*
+  for (let i = 0; i < 20; i++) {
+    explosionPos[i] = createVector(0, 700);
+    explosionVel[i] = createVector(random(-5, 5), random(0, 5));
+    explosion[i] = new Explosion(explosionPos[i], explosionVel[i], explosionAcc[i]);
+  }
+  */
 }
+
 
 /*
 Draw loop of the project
@@ -163,12 +249,9 @@ Draw loop of the project
 function draw() {
   // Code for Scene 1 of the project
   /*
-  spikes = new SceneOneSpikes(-25, -50, 850, 0);
-  balloon = new Person(60, 400, 75);
-
   background(0);
-  balloon.display();
   spikes.display();
+  circularObject(person);
   */
-  const ctx = canvas.getContext('2d');
+  sceneOne();
 }
