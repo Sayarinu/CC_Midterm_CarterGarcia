161b6ad934aa1c2e211b7668bf0fe698646c0b15
Sayarinu <bikabattles@gmail.com>
2022-10-31 19:32:10 -0400

    Added Comments and Removed Excess Code
    Added Comments and Removed Excess Code

--- sketch.js
+++ sketch.js
@@ -31,18 +31,18 @@ Adjective: Abrasive
 /*
 Globals:
 */
-let angle = 0;
+let angle = -180; // Starts the angle at -180 degrees for scene 1
 
-let bits = []
+let bits = [] // array that holds the bit objects
 
-let positions = []
-let velocities = []
-let accelerations = []
+let positions = [] // holds the position of the bits
+let velocities = [] // holds the velocity of the bits
+let accelerations = [] // holds the acceleration of the bits
 /*
 Class Definitions:
 */
 
-
+// The bits that explode when the ball does
 class Bit {
   constructor(position, velocity, acceleration) {
     this.position = position;
@@ -51,36 +51,21 @@ class Bit {
     this.maxspeed = 10;
   }
 
-  display(red, green, blue) {
+  display(red, green, blue) { // Display function passed colors to create the color of the image we want
     stroke(0);
-    fill(red, green, blue);
-    if (this.position.y < 800) {
+    fill(red, green, blue); // fills in the passed colors
+    if (this.position.y < 800) { // when they go off screen stop making them
       circle(this.position.x, this.position.y, 25);
     } 
   }
 
-  update() {
+  update() { // add the velocity vector to create speed
     this.position.add(this.velocity);
   }
 
-  accelerate() {
+  accelerate() { // accelerate as they go
     this.velocity.mult(1.1);
     this.velocity.limit(this.maxspeed);
-  }
-}
-
-
-
-class Saw { 
-  constructor(x, y, size) {
-    this.x = x;
-    this.y = y;
-    this.size = size;
-  }
-
-  display() {
-    rect(this.x, this.y, this.size, this.size);
-    rotate(Math.PI / 6);
   }
 }
 
@@ -88,28 +73,23 @@ class Saw {
 // They will be represented as a circle in the drawing in an almost balloon like feel
 // Scared of being popped
 
-class Person {
-  constructor(x, y, size) {
+class Person { // orb object in scene 1, will have functionality in some of the other scenes
+  constructor(x, y, size) { // Constructor with size and position
     this.x = x;
     this.y = y;
     this.size = size;
   }
 
-  display() {
+  display() { // Displays the ball (person in the context of the sketch)
     fill('red');
     stroke('red');
     ellipse(this.x, this.y, this.size);
     
   }
-
-  update(x, y) {
-    this.x += x;
-    this.y += y;
-  }
 }
 
 // Class to define the spikes on the first scene (this does most of the work)
-class SceneOneSpikes {
+class SceneOneSpikes { // Creates spikes on the bottom and the saw object for scene 1
   constructor(x, y, lowerY, offset) {
     this.x = x;
     this.y = y;
@@ -159,7 +139,7 @@ class SceneOneSpikes {
 Function Definitions:
 */
 
-function sceneOne() {
+function sceneOne() { // Does the code for scene one
   background(0);
   spikes.display();
   if (angle <= 44) {
@@ -168,13 +148,13 @@ function sceneOne() {
     let opacity = random(200, 255);
     stroke(255, 0, 0, opacity);
     fill(255, 0, 0, opacity);
-    if (millis() < 2000) {
+    if (millis() < 5000) {
       ellipse(400, 600, 100, 100);
       lineFlurry();
-    } else if (millis() < 5000 && millis() > 2000) {
+    } else if (millis() < 8000 && millis() > 5000) {
       ellipse(400 + (random(-10, 10)), 600 + random(-10, 10), 100, 100);
       lineFlurry();
-    } else if (millis() > 5000 && millis() < 10000) {
+    } else if (millis() > 8000 && millis() < 13000) {
       for (let i = 0; i < 50; i += 1)  {
         bits[i].update();
         bits[i].display(random(0, 255), random(0, 25), random(0,25));
@@ -183,18 +163,19 @@ function sceneOne() {
       sceneTwo();
     }
   }
-/*
-  for (let i = 0; i < 100; i+= 1) {
-    explosion[i].update();
-    explosion[i].display();
-  }
-*/
 }
-function sceneTwo() {
+
+function sceneTwo() { // Code for scene two
   background(255);
 }
 
-function circularObject(obj) {
+function sceneThree() { // Code for scene three
+
+}
+
+// Moves the object in the circle no matter what object it is   
+//(passed any object with a display fucntion)
+function circularObject(obj) { 
   translate(400, 470);
   stroke(255);
   rotate(angle);
@@ -203,13 +184,13 @@ function circularObject(obj) {
   angle += 1;
 }
 
-function lineFlurry() {
+function lineFlurry() { // Flurry of lines for scene 1
   const ctx = canvas.getContext('2d');
   // set line stroke and line width
   ctx.strokeStyle = "#"+((1<<24)*Math.random()|0).toString(16);
   ctx.lineWidth = 5;
   
-      // draw a red line
+  // draw a red line
   for (let i = 0; i < 10; i++) {
     ctx.beginPath();
     ctx.moveTo(400, 650);
@@ -218,51 +199,21 @@ function lineFlurry() {
     ctx.closePath();
   }
 }
-
-/*
-function drawLine(x1, y1, x2, y2, stroke, ctx) {
-  ctx.strokeStyle = 'red';
-  ctx.lineWidth = stroke;
-  ctx.beginPath();
-  ctx.moveTo(x1, y1);
-  ctx.lineTo(x2, y2);
-  ctx.stroke();
-}
-
-function drawCircle(x1, y1, radius, stroke, ctx) {
-  ctx.strokeStyle = 'red';
-  ctx.lineWidth = stroke;
-  ctx.beginPath();
-  ctx.arc(x1, y1, radius, 0, 2 * Math.PI);
-  ctx.stroke();
-}
-
-function fillCircle(x, y, radius, ctx) {
-  ctx.fillStyle = 'red';
-  ctx.beginPath();
-  ctx.arc(x, y, radius, 0, 2 * Math.PI);
-  ctx.fill();
-}
-*/
+// Globally initialized Objects for scene 1
 person = new Person(100, 100, 100);
 spikes = new SceneOneSpikes(-25, -50, 850, 0);
 
-
-function setup() {
-  createCanvas(800, 800);
-  angleMode(DEGREES);
+// Setup function
+function setup() { 
+  createCanvas(800, 800); // Canvas size is 800 x 800
+  angleMode(DEGREES); // Sets our angles to degrees for easier math
   background(0);
-  /*
-  for (let i = 0; i < 20; i++) {
-    explosionPos[i] = createVector(0, 700);
-    explosionVel[i] = createVector(random(-5, 5), random(0, 5));
-    explosion[i] = new Explosion(explosionPos[i], explosionVel[i], explosionAcc[i]);
-  }
-  */
-  for (let i = 0; i <= 50; i += 1) {
-    positions[i] = createVector(400, 650);
-    velocities[i] = createVector(random(-1, 1), -random(2, 5));
-    bits[i] = new Bit(positions[i], velocities[i], accelerations[i], red, green, blue);
+
+  for (let i = 0; i <= 50; i += 1) { // Fills our arrays of bits
+    positions[i] = createVector(400, 650); // Creates the spawn point of the balls
+    velocities[i] = createVector(random(-1, 1), -random(2, 5)); // Makes them move upward in a cone
+    // Creates the new bit object and stores it in the array
+    bits[i] = new Bit(positions[i], velocities[i], accelerations[i], red, green, blue); 
   }
 }
 
@@ -271,12 +222,7 @@ function setup() {
 Draw loop of the project
 */
 
-function draw() {
+function draw() { 
   // Code for Scene 1 of the project
-  /*
-  background(0);
-  spikes.display();
-  circularObject(person);
-  */
-  sceneOne();
+  sceneOne(); // Calls Scene One
 }
